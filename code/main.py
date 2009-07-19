9b3d41371259aa5dc38b2a952658887941ad23f7
Josh Wolfe <thejoshwolfe@gmail.com>
2009-07-19 09:08:58 -0700

    generates a random (really random) chord progression and prints to console

--- code/main.py
+++ code/main.py
@@ -1,15 +1,38 @@
 
 import sys
+import random
+from functools import reduce
+
+#imports
 sys.path.append("code")
 from imports import *
 
 
+# make a random 4-bar chord progression
 
-chords = list(data.chords.values())
-chords.sort(key=lambda chord: chord.name)
+progression = []
+for _ in range(4):
+	progression.append(list(data.chords.values())[random.randint(0,len(data.chords)-1)])
 
-for chord in chords:
-	print(chord)
+# end with a 1 chord
+progression.append(data.chords["1"])
+
+
+# print to console
+print("random chord progression:")
+print()
+print("chord\ttension")
+for c in progression:
+	print(c.name + "\t" + c.tension * "*")
+print("total tension: " + str(reduce(lambda acc, c: acc + c.tension, progression, 0)))
+
+
+
+## print the list of chords
+#chords = list(data.chords.values())
+#chords.sort(key=lambda chord: chord.name)
+#for chord in chords:
+#	print(chord)
 
 
 
