d2cd02d8f761a70093441d60660d6bfe1749be36
Josh Wolfe <thejoshwolfe@gmail.com>
2009-07-19 23:41:08 -0700

    where i got so far. going to abandon this now.

--- midigen.py
+++ midigen.py
@@ -1,33 +1,36 @@
 
 # http://www.sonicspot.com/guide/midifiles.html
 
-def int2str(i):
-	return chr(i >> 24 & 0xFF) + chr(i >> 16 & 0xFF) + chr(i >> 8 & 0xFF) + chr(i >> 0 & 0xFF)
+def int2ba(i):
+	return [i >> 24 & 0xFF, i >> 16 & 0xFF, i >> 8 & 0xFF, i >> 0 & 0xFF]
 
-def short2str(i):
-	return chr(i >> 8 & 0xFF) + chr(i >> 0 & 0xFF)
+def short2ba(i):
+	return [i >> 8 & 0xFF, i >> 0 & 0xFF]
 
+def str2ba(s):
+	return [ord(c) for c in s]
 
-midistr = ""
+# array of bytes
+buffer = []
 
 # header
-midistr += "MThd"             # chunk ID
-midistr += int2str(6)         # chunk size
-midistr += short2str(0)       # format type
-midistr += short2str(1)       # number of tracks
-midistr += short2str(0x01E0)  # time division
+buffer += str2ba("MThd")     # chunk ID
+buffer += int2ba(6)          # chunk size
+buffer += short2ba(0)        # format type
+buffer += short2ba(1)        # number of tracks
+buffer += short2ba(0x01E0)   # time division
 
 
 # track 1 (the only track)
-trackstr = ""
+trackbuffer = []
 
 
-midistr += "MTrk"                 # track ID
-midistr += int2str(len(trackstr)) # chunk size
-midistr += trackstr
+buffer += str2ba("MTrk")         # track ID
+buffer += int2ba(len(trackbuffer)) # chunk size
+buffer += trackbuffer
 
 midifile = open("out.midi", "w")
-midifile.write(midistr)
+midifile.write("".join([chr(b) for b in buffer]))
 midifile.close()
 
 
